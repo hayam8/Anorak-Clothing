ba859be339c1ca9003f32ace6dbb800ed5e452bb
hayam8 <hayam8@uw.edu>
2019-10-09 09:05:48 -0700

    NavBar component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/home/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import NavBar from "./components/nav-bar/NavBar";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
---
+++ src/components/nav-bar/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./nav-bar.scss";
+
+const NavBar = () => {
+  return (
+    <div className='nav-bar'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='nav-bar-options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
