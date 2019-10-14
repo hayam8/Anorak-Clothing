fb88846ec9693134d72aa383aff5bdd6e4efd7c3
hayam8 <hayam8@uw.edu>
2019-10-14 10:53:06 -0700

    CategoryPage component added and routes in ShopPage component

---
+++ src/pages/category/CategoryPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CollectionItem from "../../components/collection-item/CollectionItem";
+
+import "./category.scss";
+
+const CategoryPage = ({ match }) => {
+  return (
+    <div className='category'>
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  );
+};
+
+export default CategoryPage;
--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
+import CategoryPage from "../category/CategoryPage";
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div className='shop-page'>
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoriId`} component={CategoryPage} />
     </div>
   );
 };
