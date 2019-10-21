afb895d31e949da9220a961d0c129493baa168e0
hayam8 <hayam8@uw.edu>
2019-10-20 21:14:10 -0700

    Stripe button component integrated with axios for payment

--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,15 +1,31 @@
 import React from "react";
+import axios from "axios";
 import StripeCheckout from "react-stripe-checkout";
 
 const StripeButton = ({ price }) => {
   // integer dollar amount multiplied by 100 to represent price in cents
-  const priceStripe = price * 100;
+  const priceForStripe = price * 100;
 
   const publishableKey = "pk_test_YY2J9XlCt3UHKqoNGEI5Sq5600zaYCiCko";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(res => {
+        alert("Payment Successful!");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card"
+        );
+      });
   };
 
   return (
@@ -20,7 +36,7 @@ const StripeButton = ({ price }) => {
       shippingAddress
       image='http://svgshare.com/i/FWh.svg'
       description={`Your total is $${price}`}
-      amount={priceStripe}
+      amount={priceForStripe}
       panelLabel='Pay Now'
       token={onToken}
       stripeKey={publishableKey}
